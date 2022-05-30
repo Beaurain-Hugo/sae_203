98013447ce8db9834d60ca8ae449f6773a672eaf
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-05-30 14:07:35 +0200

    Premier commit et header

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <header class="bg-gray-700 py-3">
+    <nav class="flex hidden flex-initial flex-wrap justify-center justify-between gap-2 sm:flex">
+      <ul class="flex text-gray-300">
+        <li class="pl-2"><router-link to="/">Accueil</router-link></li>
+        <li class="pl-2"><router-link to="/programme">Programme</router-link></li>
+        <li class="pl-2"><router-link to="/billetterie">Billetterie</router-link></li>
+        <li class="pl-2"><router-link to="/artistes">Artistes</router-link></li>
+        <li class="pl-2"><router-link to="/partenaires">Partenaires</router-link></li>
+      </ul>
+      <ul class="flex">
+        <li class="pr-2"><router-link to="#" class="rounded-lg bg-red-500 py-1 px-2 text-white">SE CONNECTER</router-link></li>
+        <li class="pr-2"><router-link to="#" class="rounded-lg border-2 border-red-200 py-1 px-2 text-red-500">S'INSCRIRE</router-link></li>
+      </ul>
+    </nav>
+    <div class="flex justify-between">
+      <button class="relative z-50 text-xl sm:hidden">
+        <MenuIcon class="w- h-10" />
+        <span class="sr-only">Menu</span>
+      </button>
+      <button
+        class="relative text-xl text-gray-300 sm:hidden"
+        aria-haspopup="true"
+        aria-controls="menu"
+        :aria-expanded="menuOuvert"
+        @click="menuOuvert = !menuOuvert"
+      >
+        <MenuIcon class="w- h-10" />
+      </button>
+    </div>
+    <div
+      id="menu"
+      class="
+        fixed
+        inset-0
+        flex
+        translate-x-full
+        items-center
+        justify-center
+        bg-gray-700
+        motion-safe:transition-transform motion-safe:duration-1000
+      "
+      :class="{ 'translate-x-0': menuOuvert }"
+    >
+      <button
+        class="absolute top-3 right-0 text-xl text-gray-300 sm:hidden"
+        aria-haspopup="true"
+        aria-controls="menu"
+        :aria-expanded="menuOuvert"
+        @click="menuOuvert = !menuOuvert"
+      >
+        <XIcon class="w- h-10" />
+      </button>
+      <nav>
+        <ul class="text-center text-gray-300">
+          <li class="my-4"><router-link to="/">Accueil</router-link></li>
+          <li class="my-4"><router-link to="/programme">Programme</router-link></li>
+          <li class="my-4"><router-link to="/billetterie">Billetterie</router-link></li>
+          <li class="my-4"><router-link to="/artistes">Artistes</router-link></li>
+          <li class="my-4"><router-link to="/partenaires">Partenaires</router-link></li>
+        </ul>
+        <ul class="text-center">
+          <li class="mb-4"><router-link to="#" class="rounded-lg bg-red-500 py-1 px-2 text-white">SE CONNECTER</router-link></li>
+          <li class="mt-6">
+            <router-link to="#" class="rounded-lg border-2 border-red-200 py-1 px-2 text-red-500">S'INSCRIRE</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+
+  <footer></footer>
+</template>
+
+<script>
+import { MenuIcon, XIcon } from "@heroicons/vue/solid";
+export default {
+  name: "App",
+  data() {
+    return {
+      menuOuvert: false,
+    };
+  },
+  beforeMount() {
+    this.$router.afterEach(() => (this.menuOuvert = false));
+  },
+  components: { MenuIcon, XIcon },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AccueilView from '../views/AccueilView.vue'
+import ProgrammeView from '../views/ProgrammeView.vue'
+import BilletterieView from '../views/BilletterieView.vue'
+import ArtistesView from '../views/ArtistesView.vue'
+import PartenairesView from '../views/PartenairesView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'Accueil', component: AccueilView },
+    { path: '/programme', name: 'Programme', component: ProgrammeView },
+    { path: '/billetterie', name: 'Billetterie', component: BilletterieView },
+    { path: '/artistes', name: 'Artistes', component: ArtistesView },
+    { path: '/partenaires', name: 'Partenaires', component: PartenairesView },
+    // ici les autre routes
+  ]
+})
+
+export default router
