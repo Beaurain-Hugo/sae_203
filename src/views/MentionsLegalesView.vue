0dc608ce01b956b5f4790c98c265bece68577fa0
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-06-01 11:33:55 +0200

    Modification mentions légaes

--- src/views/MentionsLegalesView.vue
+++ src/views/MentionsLegalesView.vue
@@ -6,26 +6,26 @@
           <h1 class="font-tapestry text-3xl">Mentions légales</h1>
           <div>
             <h2 class="pt-4 font-tapestry text-2xl">1. Présentation du site internet</h2>
-            <p>
+            <p class="pt-2">
               En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux
               utilisateurs du site internet https://gold-rock.beaurain-hugo.fr/ l'identité des différents intervenants dans le cadre de sa
               réalisation et de son suivi:
             </p>
-            <p>
+            <p class="pt-2">
               ropriétaire : Beaurain – 25200 Montbéliard Responsable publication : https://gold-rock.beaurain-hugo.fr/ Le responsable
               publication est une personne physique ou une personne morale. Webmaster : Beaurain – https://gold-rock.beaurain-hugo.fr/
               Hébergeur : ovh – 2 rue Kellermann 59100 Roubaix 1007
             </p>
-            <p>Délégué à la protection des données : Beaurain – hugo.beaurain@edu.univ-fcomte.fr</p>
+            <p class="pt-2">Délégué à la protection des données : Beaurain – hugo.beaurain@edu.univ-fcomte.fr</p>
           </div>
           <div>
             <h2 class="pt-4 font-tapestry text-2xl">2. Conditions générales d’utilisation du site et des services proposés</h2>
-            <p>
+            <p class="pt-2">
               Le Site constitue une œuvre de l’esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des
               Réglementations Internationales applicables. Le Client ne peut en aucune manière réutiliser, céder ou exploiter pour son
               propre compte tout ou partie des éléments ou travaux du Site.
             </p>
-            <p>
+            <p class="pt-2">
               L’utilisation du site https://gold-rock.beaurain-hugo.fr/ implique l’acceptation pleine et entière des conditions générales
               d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment,
               les utilisateurs du site https://gold-rock.beaurain-hugo.fr/ sont donc invités à les consulter de manière régulière.
@@ -33,14 +33,14 @@
           </div>
           <div>
             <h2 class="pt-4 font-tapestry text-2xl">3. Description des services fournis</h2>
-            <p>
+            <p class="pt-2">
               Le site internet https://gold-rock.beaurain-hugo.fr/ a pour objet de fournir une information concernant l’ensemble des
               activités de la société. https://gold-rock.beaurain-hugo.fr/ s’efforce de fournir sur le site
               https://gold-rock.beaurain-hugo.fr/ des informations aussi précises que possible. Toutefois, il ne pourra être tenu
               responsable des oublis, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des
               tiers partenaires qui lui fournissent ces informations.
             </p>
-            <p>
+            <p class="pt-2">
               Toutes les informations indiquées sur le site https://gold-rock.beaurain-hugo.fr/ sont données à titre indicatif, et sont
               susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site https://gold-rock.beaurain-hugo.fr/ ne sont pas
               exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne
