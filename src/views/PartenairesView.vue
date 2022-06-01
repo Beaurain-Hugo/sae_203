b5db89bc229d983a9c3270a0652d3b4dbcb6ee41
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-06-01 10:21:10 +0200

    Correction page billetterie et début page partenaire

--- src/views/PartenairesView.vue
+++ src/views/PartenairesView.vue
@@ -1,6 +1,9 @@
 <template>
-  <main>
-    <h1 class="text-2xl">Bonjour</h1>
+  <main class="bg-red-50">
+    <div class="p-8 text-center font-tapestry">
+      <h1 class="text-3xl">Ce festival n'existerait pas sans nos partenaires !</h1>
+      <h2 class="pt-4 text-2xl">Un grand merci à eux qui ont acceptés de nous faire confiance !</h2>
+    </div>
   </main>
 </template>
 
