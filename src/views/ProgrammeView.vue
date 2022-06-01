ed6c24d125abd14041e0dc23ad01283f2b49031a
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-06-01 10:08:38 +0200

    Page programme

--- src/views/ProgrammeView.vue
+++ src/views/ProgrammeView.vue
@@ -1,6 +1,99 @@
 <template>
-  <main>
-    <h1 class="text-2xl">Bonjour</h1>
+  <main class="bg-red-50">
+    <div class="p-8">
+      <div class="text-center font-tapestry">
+        <h1 class="text-3xl">Besoin de savoir à quel moment vos artistes préférés sont sur scène ?</h1>
+        <h2 class="pt-4 text-2xl">Avec le programme, vous ne pourrez pas les rater !</h2>
+      </div>
+      <div class="flex justify-around">
+        <table class="table-auto text-center text-xl">
+          <thead>
+            <tr>
+              <th>Artiste</th>
+              <th>Jour</th>
+              <th>Heure</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Vendredi</td>
+              <td>20h</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Vendredi</td>
+              <td>21h30</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Vendredi</td>
+              <td>23h</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Samedi</td>
+              <td>17h00</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Samedi</td>
+              <td>18h30</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Samedi</td>
+              <td>20h00</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Samedi</td>
+              <td>21h30</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Samedi</td>
+              <td>23h00</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Samedi</td>
+              <td>00h30</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Dimanche</td>
+              <td>17h00</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Dimanche</td>
+              <td>18h30</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Dimanche</td>
+              <td>20h00</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Dimanche</td>
+              <td>21h30</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Dimanche</td>
+              <td>23h00</td>
+            </tr>
+            <tr>
+              <td>Prénom Nom</td>
+              <td>Dimanche</td>
+              <td>00h30</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </main>
 </template>
 
