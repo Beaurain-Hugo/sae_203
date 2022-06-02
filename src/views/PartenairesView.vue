0090201658b7bb6e30cbeaeddceaccdf53c10a2b
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-06-02 08:52:05 +0200

    Modification des partenaires

--- src/views/PartenairesView.vue
+++ src/views/PartenairesView.vue
@@ -7,14 +7,19 @@
       </div>
 
       <div class="flex flex-col pt-4" v-for="part in listPart" :key="part.id">
-        <p class="font-tapestry text-4xl">{{ part.nom }}</p>
-        <p class="font-radio-canada text-xl">{{ part.desc }}</p>
+        <p class="font-tapestry text-4xl"><input type="text" v-model="part.nom" /></p>
+        <p class="font-radio-canada text-xl"><input type="text" v-model="part.desc" /></p>
+
+        <button type="button" @click.prevent="updatePart(part)" title="Modification"><i>Modifier</i></button>
+        <button type="button" @click.prevent="deletePart(part)" title="Suppression"><i>Supprimer</i></button>
       </div>
       <form action="">
         <p>Nouveau partenaire</p>
         <div>
           <div><span>Nom</span></div>
           <input type="text" v-model="nom" required />
+          <div><span>Description</span></div>
+
           <input type="text" v-model="desc" required />
           <button type="button" @click="createPart()" title="Création"><i>Créer</i></button>
         </div>
@@ -60,6 +65,14 @@ export default {
         desc: this.desc,
       });
       console.log("document crée avec le id : ", docRef.id);
+    },
+    async updatePart(part) {
+      const firestore = getFirestore();
+      const docRef = doc(firestore, "partenaires", part.id);
+      await updateDoc(docRef, {
+        nom: part.nom,
+        desc: part.desc,
+      });
     },
   },
 };
